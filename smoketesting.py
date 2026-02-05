1a41f0cad8cfc320f459603ee3450246254ca916
sakar kharel <97268953+sakarkharel@users.noreply.github.com>
2026-02-05 11:08:01 +0545

    first commit

---
+++ smoketesting.py
@@ -0,0 +1,69 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+driver = webdriver.Chrome()
+driver.get("https://www.saucedemo.com/")
+title = driver.title
+driver.implicitly_wait(5)
+
+def login():
+    text_username = driver.find_element(by=By.ID,value ="user-name")
+    text_password = driver.find_element(by=By.ID, value = "password")
+    submit_button = driver.find_element(by=By.ID, value = "login-button")
+    
+    text_username.send_keys("standard_user")
+    text_password.send_keys("secret_sauce")
+    submit_button.click()
+    time.sleep(10)
+
+def homepage():
+    button_addcartbackpack = driver.find_element(by= By.ID, value ="add-to-cart-sauce-labs-backpack")
+    button_addcartbikelight = driver.find_element(by=By.ID, value = "add-to-cart-sauce-labs-bike-light")
+
+    button_addcartbackpack.click()
+    button_addcartbikelight.click()
+    
+
+    button_cart = driver.find_element(by=By.CLASS_NAME, value="shopping_cart_link")
+    button_cart.click()
+    time.sleep(5)
+
+def cart():
+    button_checkout = driver.find_element(by=By.ID, value= "checkout")
+    button_checkout.click()
+    time.sleep(5)
+
+def checkout_stepone():
+    text_firstname = driver.find_element(by=By.ID, value = "first-name")
+    text_lastname = driver.find_element(by=By.ID, value = "last-name")
+    text_postalcode = driver.find_element(by=By.ID, value = "postal-code")
+    continue_button = driver.find_element(by=By.ID, value= "continue")
+
+    text_firstname.send_keys("Ram")
+    text_lastname.send_keys("Bahadur")
+    text_postalcode.send_keys("44800")
+    time.sleep(5)
+    continue_button.click()
+    time.sleep(5)
+
+def checkout_steptwo():
+    button_checkouttwo = driver.find_element(by=By.ID, value = "finish")
+    button_checkouttwo.click()
+    time.sleep(5)
+
+
+def main():
+    login()
+    homepage()
+    cart()
+    checkout_stepone()
+    checkout_steptwo()
+    driver.quit()
+
+
+main()
+
+
+
