1d464ae244a5ad6ab4a4256cfc2c1ef8316f22d8
sakar kharel <97268953+sakarkharel@users.noreply.github.com>
2026-03-18 20:20:47 +0545

    chore: disable non-working function in edge_case.py

--- edge_case.py
+++ edge_case.py
@@ -1,97 +1,92 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-URL = "https://www.saucedemo.com/"
-
-
-def open_page(driver):
-    driver.get(URL)
-
-
-def login(driver, username, password):
-    wait = WebDriverWait(driver, 10)
-
-    username_box = wait.until(
-        EC.presence_of_element_located((By.ID, "user-name"))
-    )
-    password_box = driver.find_element(By.ID, "password")
-    login_button = driver.find_element(By.ID, "login-button")
-
-    username_box.clear()
-    password_box.clear()
-
-    username_box.send_keys(username)
-    password_box.send_keys(password)
-    login_button.click()
+from selenium.webdriver.common.by import By 
+import time 
 
 
 def sql_injection(driver):
-    open_page(driver)
-    login(driver, "' OR '1'='1", "anything")
+    text_username = driver.find_element(by=By.ID, value="user-name")
+    text_password = driver.find_element(by=By.ID, value = "password")
+    submit_button = driver.find_element(by=By.ID, value = "login-button")
+
+    text_username.send_keys("' OR '1'='1")
+    text_password.send_keys("anything")
+    submit_button.click()
+    time.sleep(3)
 
 
 def special_char(driver):
-    open_page(driver)
-    login(driver, "!@#$%^&*()", "!@#$%^&*()")
+    driver.get("https://www.saucedemo.com/")
+    text_username = driver.find_element(by=By.ID, value = "user-name")
+    text_password = driver.find_element(by=By.ID, value = "password")
+    submit_button = driver.find_element(by=By.ID, value = "login-button")
 
+    text_username.send_keys("!@#$%^&*()")
+    text_password.send_keys("!@#$%^&*()")
+    submit_button.click()
+    time.sleep(3)
 
 def case_sensitive(driver):
-    open_page(driver)
-    login(driver, "STANDARD_USER", "SECRET_SAUCE")
+    driver.get("https://www.saucedemo.com/")
+    text_username = driver.find_element(by=By.ID, value = "user-name")
+    text_password = driver.find_element(by=By.ID, value = "password")
+    submit_button = driver.find_element(by=By.ID, value = "login-button")
 
+    text_username.send_keys("STANDARD_USER")
+    text_password.send_keys("SECRET_SAUCE")
+    submit_button.click()
+    time.sleep(3)
 
 def lead_trail_space(driver):
-    open_page(driver)
-    login(driver, " standard_user ", " secret_sauce ")
+    driver.get("https://www.saucedemo.com/")
+    text_username = driver.find_element(by=By.ID, value = "user-name")
+    text_password = driver.find_element(by=By.ID, value = "password")
+    submit_button = driver.find_element(by=By.ID, value = "login-button")
+
+    text_username.send_keys(" standard_user ")
+    text_password.send_keys(" secret_sauce ")
+    submit_button.click()
+    time.sleep(3)
+
+def long_input(driver, long_username,long_password ):
+    driver.get("https://www.saucedemo.com/")
+    text_username = driver.find_element(by=By.ID, value = "user-name")
+    text_password = driver.find_element(by=By.ID, value = "password")
+    submit_button = driver.find_element(by=By.ID, value = "login-button")
+
+    text_username.send_keys(long_username)
+    text_password.send_keys(long_password)
+    submit_button.click()
+    time.sleep(3)
+
+#refreshing page after entering credentials 
+def refresh(driver):
+    driver.get("https://www.saucedemo.com/")
+    text_username = driver.find_element(by=By.ID, value = "user-name")
+    text_password = driver.find_element(by=By.ID, value = "password")
+    submit_button = driver.find_element(by=By.ID, value = "login-button")
+
+    text_username.send_keys("standard_user")
+    text_password.send_keys("secret_sauce")
+    driver.refresh()
+    time.sleep(3)
+    submit_button.click()
 
 
-def long_input(driver):
-    open_page(driver)
+def main ():
+    driver = webdriver.Chrome()
+    driver.get("https://www.saucedemo.com/")
+    title = driver.title
+    driver.implicitly_wait(5)
     long_username = "a" * 1000
     long_password = "b" * 1000
-    login(driver, long_username, long_password)
+    sql_injection(driver)
+    special_char(driver)
+    case_sensitive(driver)
+    lead_trail_space(driver)
+    long_input(driver, long_username, long_password)
+    # refresh(driver)
 
 
-def refresh_test(driver):
-    open_page(driver)
-
-    username = driver.find_element(By.ID, "user-name")
-    password = driver.find_element(By.ID, "password")
-
-    username.send_keys("standard_user")
-    password.send_keys("secret_sauce")
-
-    driver.refresh()
-
-    login_button = driver.find_element(By.ID, "login-button")
-    login_button.click()
-
-
-def rapid_click(driver):
-    open_page(driver)
-    login(driver, "standard_user", "secret_sauce")
-
-    for _ in range(5):
-        driver.find_element(By.ID, "login-button").click()
-
-
-def main():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-
-    try:
-        sql_injection(driver)
-        special_char(driver)
-        case_sensitive(driver)
-        lead_trail_space(driver)
-        long_input(driver)
-        refresh_test(driver)
-        rapid_click(driver)
-
-    finally:
-        driver.quit()
 
 
 if __name__ == "__main__":
