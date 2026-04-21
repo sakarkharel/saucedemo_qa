03a05171af8faaed80267d4132e7003e9ea19a68
sakar kharel <97268953+sakarkharel@users.noreply.github.com>
2026-04-21 16:15:07 +0545

    test(inventory): add test cases; remove-from-cart not working

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -2,5 +2,88 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from pages.base_page import BasePage
+from selenium.common.exceptions import TimeoutException
 
-class 
+class InventoryPage:
+
+    PRODUCT_NAMES = (By.CLASS_NAME, "inventory_item_name")
+    PRODUCT_PRICES = (By.CLASS_NAME, "inventory_item_price")
+    ADD_TO_CART_BUTTONS = (By.XPATH, "//button[starts-with(@id, 'add-to-cart')]")
+    REMOVE_BUTTONS = (By.XPATH, "//button[starts-with(@id, 'remove')]")
+    CART_BADGE = (By.CLASS_NAME, "shopping_cart_badge")
+    CART_ICON = (By.CLASS_NAME, "shopping_cart_link")
+    SORT_DROPDOWN = (By.CLASS_NAME, "product_sort_container")
+    MENU_BUTTON = (By.ID, "react-burger-menu-btn")
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
+
+    
+    #for data of items 
+
+    def get_product_names(self):
+        elements = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_all_elements_located(self.PRODUCT_NAMES)
+        )
+        return [e.text for e in elements] # returning the names of all items 
+    
+    def get_product_prices(self):
+        elements = WebDriverWait(self.driver,10).until(
+            EC.visibility_of_all_elements_located(self.PRODUCT_PRICES)
+        )
+        return [e.text for e in elements]
+    
+    
+    # Doing actions in cart 
+    def add_first_item_to_cart(self):
+        buttons = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.ADD_TO_CART_BUTTONS)
+        )
+        buttons.click()
+
+    # def add_item_by_index(self, index):
+    #     buttons = WebDriverWait(self.driver, 10).until(
+    #         EC.visibility_of_all_elements_located(self.ADD_TO_CART_BUTTONS)
+    #     )
+    #     buttons[index].click()
+    
+    def remove_first_item(self):
+        buttons = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.REMOVE_BUTTONS)
+        )
+        buttons.click()
+    
+    def get_cart_count(self):
+        try:
+            badge=self.wait.until(
+                EC.visibility_of_element_located(self.CART_BADGE)
+            )
+            return badge.text
+        except TimeoutException:
+            return "0"
+
+       #sorting the products based on prices and alphabets  
+    def sort_products(self, option_value):
+        dropdown = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(self.SORT_DROPDOWN)
+        )
+        dropdown.click()
+        dropdown.find_element(By.XPATH, f".//option[@value='{option_value}']").click()
+
+
+    # # Navigation (moving to another page)
+    # def go_to_cart(self):
+    #     self.driver.find_element(*self.CART_ICON).click()
+
+    # # sorting the products based on prices and alphabets 
+    # def sort_products(self, option_value):
+    #     dropdown = self.driver.find_element(*self.SORT_DROPDOWN)
+    #     dropdown.click()
+    #     dropdown.find_element(By.XPATH, f"//option[@value='{option_value}']").click()
+
+    # #UI
+    # def open_menu(self):
+    #     self.driver.find_element(*self.MENU_BUTTON).click()
+
+    
---
+++ tests/test_inventory.py
@@ -0,0 +1,60 @@
+import pytest 
+from pages.login_page import LoginPage
+from pages.inventory_page import InventoryPage
+
+@pytest.fixture
+def logged_in(driver):
+    login = LoginPage(driver)
+    login.load()
+    login.login("standard_user", "secret_sauce")
+    assert "inventory" in driver.current_url, "Login failed or page did not load correctly"
+    return InventoryPage(driver)
+
+#data of tiems 
+
+def test_products_are_displayed(logged_in):
+    names = logged_in.get_product_names()
+    prices = logged_in.get_product_prices()
+
+    assert len(names)>0
+    assert len(prices)>0
+
+#doing actions in cart part 
+def test_add_to_cart_updates_badge(logged_in):
+    logged_in.add_first_item_to_cart()
+    assert logged_in.get_cart_count() == "1"
+
+def test_remove_from_cart(logged_in):
+    logged_in.add_first_item_to_cart()
+    logged_in.remove_first_item()
+    # assert logged_in.get_cart_count() == "0"
+
+    logged_in.wait.until(
+        lambda d: logged_in.get_cart_count() == "0"
+    )
+
+    assert logged_in.get_cart_count() == "0"
+
+# sorting theproducts based on prices and alphabets 
+def test_sort_low_to_high(logged_in):
+    logged_in.sort_products("lohi")
+    prices = logged_in.get_product_prices()
+    prices = [float(p.replace("$", "")) for p in prices]
+    assert prices == sorted(prices)
+
+def test_sort_high_to_low(logged_in):
+    logged_in.sort_products("hilo")
+    prices = logged_in.get_product_prices()
+    prices = [float(p.replace("$", "")) for p in prices]
+    assert prices == sorted(prices, reverse=True)
+
+def test_sort_a_to_z(logged_in):
+    logged_in.sort_products("az")
+    names = logged_in.get_product_names()
+    assert names == sorted(names)
+
+def test_sort_z_to_a(logged_in):
+    logged_in.sort_products("za")
+    names = logged_in.get_product_names()
+    assert names == sorted(names, reverse=True)
+    
