53b1f04db8de4cbc30fe78ee4a23ec8eb2d6bb2e
sakar kharel <97268953+sakarkharel@users.noreply.github.com>
2026-04-21 20:46:54 +0545

    chore: add test file for cart page of saucedemo

---
+++ pages/cart_page.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from pages.base_page import BasePage
+from selenium.common.exceptions import TimeoutException
+
+
+class CartPage:
+    
