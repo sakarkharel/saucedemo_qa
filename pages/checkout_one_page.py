6b7864aff16d5012390d074957aa03fdc12a5b3a
sakar kharel <97268953+sakarkharel@users.noreply.github.com>
2026-04-22 19:29:12 +0545

    test(checkoutstepone): add test case for step one of checkout process

---
+++ pages/checkout_one_page.py
@@ -0,0 +1,64 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from pages.base_page import BasePage
+from selenium.common.exceptions import TimeoutException
+from pages.inventory_page import InventoryPage
+
+
+class CheckoutStepone:
+    CANCEL_BUTTON = (By.ID, "cancel")
+    CHECKOUT_BUTTON = (By.ID, "continue")
+    FIRST_NAME = (By.ID, "first-name")
+    LAST_NAME = (By.ID, "last-name")
+    ZIP_CODE = (By.ID, "postal-code")
+    ERROR_MESSAGE = (By.CSS_SELECTOR, "[data-test='error']")
+
+    def __init__(self,driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver,10)
+        self.inventory_page = InventoryPage(driver)
+
+
+    def checkout_stepone_cancelbutton(self):
+        cancel_button = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.CANCEL_BUTTON)
+        )
+        cancel_button.click()
+
+    def checkout_info_firstname(self, firstname):
+        info_firstname = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.FIRST_NAME)
+        )
+        info_firstname.clear()
+        info_firstname.send_keys(firstname)
+
+    
+    def checkout_info_lastname(self, lastname):
+        info_lastname = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.LAST_NAME)
+        )
+        info_lastname.clear()
+        info_lastname.send_keys(lastname)
+
+
+    def checkout_info_zipcode(self, zipcode):
+        info_zipcode = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.ZIP_CODE)
+        )
+        info_zipcode.clear()
+        info_zipcode.send_keys(zipcode)
+
+    def checkout_error(self):
+        error_element = self.wait.until(
+            EC.visibility_of_element_located(self.ERROR_MESSAGE)
+        )
+        return error_element.text
+
+    
+    def checkout_info(self):
+        checkout_info_button = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.CHECKOUT_BUTTON)
+        )
+        checkout_info_button.click()
+    
---
+++ tests/test_checkout_one.py
@@ -0,0 +1,73 @@
+import pytest 
+from pages.login_page import LoginPage
+from pages.inventory_page import InventoryPage
+from pages.cart_page import CartPage
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from pages.checkout_one_page import CheckoutStepone
+import time
+
+@pytest.fixture
+def logged_in(driver):
+    login = LoginPage(driver)
+    login.load()
+    login.login("standard_user", "secret_sauce")
+    assert "inventory" in driver.current_url, "Login failed or page did not load correctly"
+    return InventoryPage(driver)
+
+def test_cancel_button(logged_in, driver):
+    inventory_page=logged_in
+    inventory_page.add_first_item_to_cart()
+    assert inventory_page.get_cart_count() == "1"
+
+    #cart clicking 
+    cart_page = CartPage(driver)
+    cart_page.click_on_cart()
+    time.sleep(5)
+    assert "cart" in driver.current_url
+
+    #now remving item
+    cart_page.checkout()
+
+    # finally cancel button click 
+
+    checkoutone_page = CheckoutStepone(driver)
+    checkoutone_page.checkout_stepone_cancelbutton()
+    time.sleep(5)
+    assert "cart" in driver.current_url
+
+
+
+def test_checkout_info_validation(logged_in, driver):
+    inventory_page = logged_in
+    inventory_page.add_first_item_to_cart()
+
+    cart_page = CartPage(driver)
+    cart_page.click_on_cart()
+    cart_page.checkout()
+
+    scenarios = [
+        {"firstname": "", "lastname": "", "zipcode": "", "expected": "First Name is required"},
+        {"firstname": "", "lastname": "Bahadur", "zipcode": "44800", "expected": "First Name is required"},
+        {"firstname": "Ram", "lastname": "", "zipcode": "44800", "expected": "Last Name is required"},
+        {"firstname": "Ram", "lastname": "Bahadur", "zipcode": "", "expected": "Postal Code is required"},
+    ]
+
+    for scenario in scenarios:
+        checkout_page = CheckoutStepone(driver)
+
+ 
+        checkout_page.checkout_info_firstname(scenario["firstname"])
+        checkout_page.checkout_info_lastname(scenario["lastname"])
+        checkout_page.checkout_info_zipcode(scenario["zipcode"])
+
+        checkout_page.checkout_info()
+
+        error_message = checkout_page.checkout_error()
+        assert scenario["expected"] in error_message
+
+        driver.refresh()
+
+
+
